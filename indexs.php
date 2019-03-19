84a8b82291a7fa702c6c6dc534f514090dff5bb8
sama69 <48472313+sama69@users.noreply.github.com>
2019-03-19 12:42:31 +0700

    Add files via upload

---
+++ indexs.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $kon = mysqli_connect("localhost","id7899380_tugas2","tugas2","id7899380_tugas2");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
